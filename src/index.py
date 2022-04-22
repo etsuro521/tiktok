66c6f4969f7ae65040d8ff18eeda437419dbcd88
etsuro <75.ef.35xy@gmail.com>
2022-04-22 15:42:29 +0900

    Paste a round image on the background for tiktok

--- src/index.py
+++ src/index.py
@@ -1,18 +1,37 @@
 from PIL import Image, ImageDraw
 
-#画像ファイル名宣言
-pic_name="let_me_know.png"
-pic_name_out="out.png"
-#追加枠ピクセル数
+im_name = "IMG_4008"
+im = Image.open("../img/"+im_name+".jpg").convert('RGBA')
+im_x,im_y= im.size
+
+if im_x > im_y:
+    im_2r = im_y
+else:
+    im_2r = im_x
+
+im_a = Image.new("L", im.size, 0)
+draw = ImageDraw.Draw(im_a)
+draw.ellipse((0,0,im_2r,im_2r),fill=255)
+im_rgba = im.copy()
+im_rgba.putalpha(im_a)
+im_rgba_crop = im_rgba.crop((0,0,im_2r,im_2r))
 
 
-im = Image.open(pic_name).convert("RGB")
-draw = ImageDraw.Draw(im)
 
-x,y = im.size
-eX, eY = int(x/5),int(x/5)
-bbox =  (x/2 - eX/2, y/2 - eY/2, x/2 + eX/2, y/2 + eY/2)
-draw.ellipse(bbox , fill=(0, 0, 0), outline=(0, 0, 0))
+mask_im = Image.new('RGBA',(900,1600),0)
+back_im = mask_im.copy()
+b_x,b_y = mask_im.size
+start_x = int((b_x - im_2r)/2)
+start_y = int((b_y - im_2r)/4)
+im_clear = Image.new("RGBA", back_im.size, (255, 255, 255, 0))
+im_clear.paste(im_rgba_crop,(start_x,start_y))
+back_im = Image.alpha_composite(back_im, im_clear)
+back_im.show()
 
 
-im.save('pillow_imagedraw.png', quality=95)
+
+
+
+
+
+
