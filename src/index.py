869f87f7f0993bd0db79ea65e54cc32e9ca333cf
etsuro <75.ef.35xy@gmail.com>
2022-05-04 21:59:21 +0900

    Completed rotated image with background 2.0

--- src/index.py
+++ src/index.py
@@ -9,23 +9,18 @@ def make_circle(im,im_2r):
     im_rgba_crop = im_rgba.crop((0,0,im_2r,im_2r))
     return im_rgba_crop
 
-
-
 def paste_to_back(pasted_im,im_2r):
-    mask_im = Image.new('RGBA',(900,1600),0)
-    back_im = mask_im.copy()
-    b_x,b_y = mask_im.size
+    origin_back_im = Image.open("../img/back.jpg")
+    back_im = origin_back_im.convert("RGBA").copy()
+    b_x,b_y = back_im.size
     start_x = int((b_x - im_2r)/2)
     start_y = int((b_y - im_2r)/4)
-    im_clear = Image.new("RGBA", back_im.size, (255, 255, 255, 0))
-    im_clear.paste(pasted_im,(start_x,start_y))
-    back_im = Image.alpha_composite(im_clear,back_im)
+    back_im.paste(pasted_im,(start_x,start_y))
     return back_im
-
 
 def main():
     im_name = "IMG_4008"
-    im = Image.open("../img/"+im_name+".jpg").convert('RGBA')
+    im = Image.open("../img/"+im_name+".jpg")
     im_x,im_y= im.size
 
     if im_x > im_y:
@@ -33,18 +28,15 @@ def main():
     else:
         im_2r = im_x
 
-    s_im = make_circle(im,im_2r)
     im_list = []
+    s_im = make_circle(im,im_2r)
     for i in range(0,360,1):
         k_im = s_im.rotate(i)
-        f_im = paste_to_back(k_im,im_2r)
-        im_list.append(f_im)
+        p_im = paste_to_back(k_im,im_2r)
+        im_list.append(p_im)
 
     im_list[0].save("../img/g_im.gif", save_all=True, append_images=im_list[1:],optimize=True, duration=100, loop=0) 
 
 if __name__ == "__main__":
     main()
-
-
-
  
--- src/index2.py
+++ src/index2.py
@@ -1,17 +1,5 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
-
-
-
-
-
-im_name = "IMG_4008"
-im = Image.open("../img/"+im_name+".jpg").convert('RGBA')
-
-im_list = []
-for i in range(0,360,1):
-    k_im = im.rotate(i)
-    im_list.append(k_im)
-
-im_list[0].save("../img/g_im.gif", save_all=True, append_images=im_list[1:],optimize=True, duration=100, loop=0) 
-
+im = Image.open("../img/back.jpg").convert("RGBA")
+print(im.mode)
+ 
